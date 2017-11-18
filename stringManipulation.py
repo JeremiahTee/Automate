d0d4e56d2e0032ba0e9b55480fc12d5898d245b4
Jeremiah <jeremiahteee@gmail.com>
2017-11-17 21:08:45 -0500

    added methods to validate

--- stringManipulation.py
+++ stringManipulation.py
@@ -14,3 +14,18 @@ Bob''')
 # multi-line commenting
 """This is a test Python program
 Written by Jeremiah Tiongson"""
+
+# validating output
+while True:
+    print('Enter your age: ')
+    age = input()
+    if age.isdecimal():
+        break
+    print('Please enter a number for you age.')
+
+while True:
+    print('Select a new password (letters and numbers only):')
+    password = input()
+    if password.isalnum():  #isalnum() for alphanumeric
+        break
+    print('Passwords can only have letters and numbers')
