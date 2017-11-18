e1c59b485da4b3dc8a1b1bd6ff3fdc7cbb87d05f
Jeremiah <jeremiahteee@gmail.com>
2017-11-17 21:01:21 -0500

    stringManipulation

---
+++ stringManipulation.py
@@ -0,0 +1,16 @@
+# raw string, ignores all escape characters
+print(r'That is Carol\'s cat.')
+print()
+
+# multi-lines
+print('''Dear Alice,
+
+Eve's cat has been arrested for catnapping, cat burglary, and extortion. 
+
+Sincerely,
+Bob''')
+
+
+# multi-line commenting
+"""This is a test Python program
+Written by Jeremiah Tiongson"""
