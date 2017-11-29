45d9bea402120338a0655d35ad4004b71625b164
Jeremiah <jeremiahteee@gmail.com>
2017-11-29 18:11:22 -0500

    a better prompt

--- greeter.py
+++ greeter.py
@@ -1,2 +1,8 @@
+"""
 name = input("Please enter your nam: ")
 print("Hello, " + name + "!")
+"""
+
+# A better prompt
+prompt = "If you tell us who you are, we can personalize the messages you see."
+prompt += "\nWhat is your first name? "
